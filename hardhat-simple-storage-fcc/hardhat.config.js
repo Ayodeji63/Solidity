477795da8dc9bc9a99815808afeb742c75160859
Ayodeji <olusanyasamson633@gmail.com>
2023-01-14 19:23:56 +0100

    simple-storage-hardhat-completed

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -2,10 +2,14 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
 /** @type import('hardhat/config').HardhatUserConfig */
-const GORERLI_RPC_URL = process.env.GORERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const API_KEY = process.env.API_KEY
+const GORERLI_RPC_URL = process.env.GORERLI_RPC_URL || "https:goerli-"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xKey"
+const API_KEY = process.env.API_KEY || "0xKey"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -14,9 +18,21 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 5,
         },
+        localNetwork: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
     },
     solidity: "0.8.17",
     etherscan: {
         apiKey: API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
     },
 }
